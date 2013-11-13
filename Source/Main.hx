f036d783f111c004f74ade70ee49cc1f7488d756
Marc Neudert <marc.neudert@gmail.com>
2013-11-13 16:49:00 +0100

    draws greyish background

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,11 +1,41 @@
 package;
 
 import flash.display.Sprite;
+import flash.events.Event;
 
 class Main extends Sprite
 {
-	public function new ()
-	{
-		super ();
-	}
+    private var background:Sprite;
+
+    public function new ()
+    {
+        super ();
+
+        initialize ();
+        construct ();
+
+        resize (stage.stageWidth, stage.stageHeight);
+        stage.addEventListener (Event.RESIZE, stage_onResize);
+    }
+
+    private function construct ():Void
+    {
+        addChild (this.background);
+    }
+
+    private function initialize ():Void
+    {
+        this.background = new Sprite();
+    }
+
+    private function resize (newWidth:Int, newHeight:Int):Void
+    {
+        this.background.graphics.beginFill (0xF2F2F2);
+        this.background.graphics.drawRect (0, 0, newWidth, newHeight);
+    }
+
+    private function stage_onResize (event:Event):Void
+    {
+        resize (stage.stageWidth, stage.stageHeight);
+    }
 }
