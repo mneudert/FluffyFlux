0cec4aa95c648b24c56239c3bb12ec20d5928c80
Marc Neudert <marc.neudert@gmail.com>
2013-11-14 19:58:17 +0100

    draws 9x9 grid

--- Source/fluffyflux/Game.hx
+++ Source/fluffyflux/Game.hx
@@ -4,6 +4,9 @@ import flash.display.Sprite;
 
 class Game extends Sprite
 {
+    private static var NUM_COLS = 9;
+    private static var NUM_ROWS = 9;
+
     private var background:Sprite;
     private var game:Game;
     private var header:Header;
@@ -19,17 +22,28 @@ class Game extends Sprite
     public function resize (newWidth:Int, newHeight:Int):Void
     {
         var sideLength = (newHeight < newWidth) ? newHeight : newWidth;
+        var boxSize    = sideLength / NUM_COLS;
 
         this.background.graphics.clear ();
-        this.background.graphics.beginFill (0x444444, 0.4);
-        this.background.graphics.drawRect (0, 0, sideLength, sideLength);
-
         this.background.x = newWidth / 2 - sideLength / 2;
+
+        for (col in 0...NUM_COLS) {
+            for (row in 0...NUM_ROWS) {
+                drawBox(col, row, boxSize);
+            }
+        }
     }
 
     private function construct ():Void
     {
         addChild (this.background);
+    }
+
+    private function drawBox (col:Int, row:Int, boxSize:Float):Void
+    {
+        this.background.graphics.lineStyle (2, 0x444444);
+        this.background.graphics.beginFill (0xFFFFFF, 0.4);
+        this.background.graphics.drawRect (col * boxSize, row * boxSize, boxSize, boxSize);
     }
 
     private function initialize ():Void
